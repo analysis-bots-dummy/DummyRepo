9730fbc315bf7a6979035073ed9e11632c237eda
Merlin Köhler <merlin-koehler@t-online.de>
2016-01-22 23:49:42 +0100

    initial commit

---
+++ src/main/App.java
@@ -0,0 +1,11 @@
+package main;
+
+
+public class App{
+
+	public static void main(String[] args){
+	
+		System.out.println("2+3=");
+	}
+	
+}
---
+++ src/main/Math.java
@@ -0,0 +1,8 @@
+package main;
+
+
+public class Math {
+	public static int addInt(int x, int y){
+		return x + y;
+	}
+}

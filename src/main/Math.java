7b77287fe30ba26d7e387b0fdbbc95aa895abe63
Merlin Köhler <merlin-koehler@t-online.de>
2016-01-24 19:44:43 +0100

    Added sample maven Project

--- src/main/Math.java
+++
@@ -1,8 +0,0 @@
-package main;
-
-
-public class Math {
-	public static int addInt(int x, int y){
-		return x + y;
-	}
-}
---
+++ src/main/java/analysisbotsdummy/App.java
@@ -0,0 +1,13 @@
+package analysisbotsdummy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
